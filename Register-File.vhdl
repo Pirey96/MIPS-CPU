f9f2d9cea0f447535d593dad6d4d4a058daaf6b8
Pirey96 <pirey8888@gmail.com>
2020-11-13 14:50:09 -0500

    register file update

--- Register-File.vhdl
+++ Register-File.vhdl
@@ -1,12 +1,9 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-
+use IEEE.numeric_std.all;
 
 -- 32 x 32 register file
 -- two read ports, one write port with write enable
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
 entity regfile is
 port( din : in std_logic_vector(31 downto 0);
  reset : in std_logic;
@@ -19,43 +16,29 @@ port( din : in std_logic_vector(31 downto 0);
  out_b : out std_logic_vector(31 downto 0));
 end regfile ;
 
-architecture registers of regfile is
-type registers is array (0 to 31) of std_logic_vector (31 downto 0)                 --array of 32 bit registers
+architecture registers_file_arch of regfile is
+type registers is array (0 to 31) of std_logic_vector (31 downto 0);                --array of 32 bit registers
 signal reg : registers;
-
+begin
 process (reset, clk, write, write_address)
 begin 
 	if (reset = '1') then 
 		for i in 0 to 31 loop
-			reg(i) <= "00000000000000000000000000000000"
+			reg(i) <= "00000000000000000000000000000000";
 		end loop;
 	end if;
 	if clk'event and clk ='1' then
 		if write = '1' then 
-			reg(conv_integer(write_address)) <= din ;
+			reg(to_integer(signed (write_address))) <= din ;
 		end if;
 	end if;
 end process;
 	
 process (read_a, read_b)
 begin
-out_a = reg(conv_integer (read_a);
-out_b = reg (conv_integer(read_b);
+	out_a <= reg(to_integer(signed (read_a)));  
+	out_b <= reg(to_integer(signed(read_b)));
 end process;
 
-end registers;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+end registers_file_arch;
 

6c9ace041735f00c215dfee3dcb3155cef7d6381
Pirey96 <pirey8888@gmail.com>
2020-11-13 21:32:44 -0500

    updates

--- Register-File.vhdl
+++ Register-File.vhdl
@@ -10,7 +10,7 @@ port( din : in std_logic_vector(31 downto 0);
  clk : in std_logic;
  write : in std_logic;
  read_a : in std_logic_vector(4 downto 0);
- read_b : in std_logic_vector(4 downto 0);
+ read_b : std_logic_vector(4 downto 0);
  write_address : in std_logic_vector(4 downto 0);
  out_a : out std_logic_vector(31 downto 0);
  out_b : out std_logic_vector(31 downto 0));
@@ -20,7 +20,7 @@ architecture registers_file_arch of regfile is
 type registers is array (0 to 31) of std_logic_vector (31 downto 0);                --array of 32 bit registers
 signal reg : registers;
 begin
-process (reset, clk, write, write_address)
+process (din, reset, clk, write, write_address)
 begin 
 	if (reset = '1') then 
 		for i in 0 to 31 loop
@@ -36,7 +36,7 @@ end process;
 	
 process (read_a, read_b)
 begin
-	out_a <= reg(to_integer(unsigned (read_a)));  
+	out_a <= reg(to_integer(unsigned(read_a)));  
 	out_b <= reg(to_integer(unsigned(read_b)));
 end process;
 
